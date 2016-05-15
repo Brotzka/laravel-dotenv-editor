5ce894c5faed018a897929491bb9822cc3c608a7
Fabian Hagen <fabianhagen87@gmail.com>
2016-05-15 09:23:33 +0200

    Added methods and improved methods
    - AddMethod
    - Deletemethod
    - getLatestBackup
    - restoreBackup
    - get Backup Versions
    - get Content of  env or env-backup

--- src/DotenvEditor.php
+++ src/DotenvEditor.php
@@ -16,20 +16,23 @@ use Symfony\Component\Process\Exception\ProcessFailedException;
 class DotenvEditor
 {
     /*
-     * TODO: Create Backups (with JSON or XML???)
-     * TODO: Restore from Backups
-     * TODO: Check the changeEnv-method
-     * TODO: Create add-method
-     * TODO: Create delete-method
-     * TODO: Create a method to return the env-entries
+     * TODO: Implement Exception
      * TODO: Create a method to return all .env-keys
      */
 
+    /*
+    |--------------------------------------------------------------------------
+    | Attributes and constructor
+    |--------------------------------------------------------------------------
+    |
+    | 
+    |
+    */
     protected $env;
     protected $backupPath;
 
     /**
-     * DotenvEditor constructor.
+     * DotenvEditor constructor
      */
     public function __construct(){
         $backupPath = config('dotenveditor.backupPath');
@@ -47,6 +50,14 @@ class DotenvEditor
         $this->backupPath = $backupPath;
     }
 
+    /*
+    |--------------------------------------------------------------------------
+    | Getter & setter
+    |--------------------------------------------------------------------------
+    |
+    | 
+    |
+    */
     /**
      * Returns the current backup-path
      *
@@ -76,6 +87,19 @@ class DotenvEditor
         return true;
     }
 
+    
+    /*
+    |--------------------------------------------------------------------------
+    | Working with backups
+    |--------------------------------------------------------------------------
+    |
+    | createBackup()
+    | getLatestBackup()
+    | restoreBackup($timestamp = NULL)
+    | getBackupVersions($formated = 1)
+    | getBackupFile($timestamp)
+    |
+    */
     /**
      * Used to create a backup of the current .env.
      * Will be assigned with the current timestamp.
@@ -83,58 +107,221 @@ class DotenvEditor
      * @return bool
      */
     protected function createBackup(){
-        $currentFile = $this->env;
-        $backupFile = $this->backupPath . time() . "_env";
-        return copy($currentFile, $backupFile);
+        return copy(
+            $this->env,
+            $this->backupPath . time() . "_env"
+        );
+    }
+
+    /**
+     * Returns the timestamp of the latest version.
+     *
+     * @return int|mixed
+     */
+    protected function getLatestBackup(){
+        $backups = $this->getVersions(0);
+        $latestBackup = 0;
+        foreach($backups as $backup){
+            if($backup > $latestBackup) {
+                $latestBackup = $backup;
+            }
+        }
+        return $latestBackup;
+    }
+
+    /**
+     * Restores the latest backup or a backup from a given timestamp.
+     * Restores the latest version when no timestamp is given.
+     *
+     * @param null $timestamp
+     * @return bool|string
+     */
+    public function restoreBackup($timestamp = NULL){
+        if($timestamp !== NULL){
+            if($this->getFile($timestamp)){
+                $file = $this->getFile($timestamp);
+            }
+        } else {
+            $file = $this->getFile($this->getLatestBackup());
+        }
+        return copy(
+            $file,
+            $this->env
+        );
+    }
+
+    /**
+     * Returns an array with all available backups.
+     * $formated on 0 will show the timestamps instead of date time format.
+     * Returns false, if no backups were found.
+     *
+     * @param int $formated
+     * @return array|bool
+     */
+    public function getBackupVersions($formated = 1){
+        $versions = array_diff(scandir($this->backupPath), array('..', '.'));
+
+        if(count($versions) > 0){
+            $output = array();
+            $c = 0;
+            foreach($versions as $version){
+                $part = explode("_", $version);
+                $output[$c] = ($formated) ? date("Y-m-d H:i:s", (int)$part[0]) : $part[0];
+                $c++;
+            }
+            return $output;
+        }
+        return false;
+    }
+
+    /**
+     * Returns filename and path for the given timestamp
+     *
+     * @param $timestamp
+     * @return bool|string
+     */
+    protected function getBackupFile($timestamp){
+        $backups = $this->getVersions(0);
+        $index = array_search($timestamp, $backups);
+        if(!$index){
+            return false;
+        }
+        return $this->backupPath . $backups[$index] . "_env";
+    }
+
+    
+    /*
+    |--------------------------------------------------------------------------
+    | Get the content
+    |--------------------------------------------------------------------------
+    |
+    | getContent($backup = NULL)
+    |
+    */
+    /**
+     * Returns the content of a given backup file
+     * or the content of the current env file.
+     *
+     * @param null $backup
+     * @return array
+     */
+    public function getContent($timestamp = NULL){
+        if($timestamp === NULL){
+            return $this->envToArray($this->env);
+        } else {
+            return $this->envToArray($this->getBackupFile($timestamp));
+        }
+    }
+
+    /**
+     * Writes the content of a env file to an array.
+     *
+     * @param $file
+     * @return array
+     */
+    protected function envToArray($file){
+        $string = file_get_contents($file);
+        $string = preg_split('/\s+/', $string);
+        $returnArray = array();
+
+        foreach($string as $one){
+            $entry = explode("=", $one, 2);
+            $returnArray[$entry[0]] = $entry[1];
+        }
+
+        return $returnArray;
     }
 
 
+    /*
+    |--------------------------------------------------------------------------
+    | Editing the env content
+    |--------------------------------------------------------------------------
+    |
+    | changeEnv($data = array())
+    |
+    */
+    protected function save($array){
+        if(is_array($array)){
+            $newArray = array();
+            $c = 0;
+            foreach($array as $key => $value){
+                $newArray[$c] = $key . "=" . $value;
+                $c++;
+            }
+
+            $newArray = implode("\n", $newArray);
+
+            file_put_contents($this->env, $newArray);
+
+            return true;
+        }
+        return false;
+    }
+
     /**
+     * Change the given values of the current env file.
+     *
      * @param array $data
      * @return bool
      */
     public function changeEnv($data = array()){
         if(count($data) > 0){
-
-            // Make a backup
             $this->createBackup();
+            $env = $this->getContent();
 
-            // Read .env-file
-            $env = file_get_contents($this->env);
-
-            // Split string on every " " and write into array
-            $env = preg_split('/\s+/', $env);;
-
-            // Loop through given data
-            foreach((array)$data as $key => $value){
-
-                // Loop through .env-data
-                foreach($env as $env_key => $env_value){
-
-                    // Turn the value into an array and stop after the first split
-                    // So it's not possible to split e.g. the App-Key by accident
-                    $entry = explode("=", $env_value, 2);
-
-                    // Check, if new key fits the actual .env-key
-                    if($entry[0] == $key){
-                        // If yes, overwrite it with the new one
-                        $env[$env_key] = $key . "=" . $value;
-                    } else {
-                        // If not, keep the old one
-                        $env[$env_key] = $env_value;
+            foreach($data as $dataKey => $dataValue){
+                foreach($env as $envKey => $envValue){
+                    if($dataKey === $envKey){
+                        $env[$envKey] = $dataValue;
                     }
                 }
             }
 
-            // Turn the array back to an String
-            $env = implode("\n", $env);
-
-            // And overwrite the .env with the new data
-            file_put_contents($this->env, $env);
-
-            return true;
+            return $this->save($env);
         } else {
             return false;
         }
+    }
+
+    /**
+     * Add data to the current env file.
+     * Data will be placed at the end.
+     * 
+     * @param array $data
+     * @return bool
+     */
+    public function addData($data = array()){
+        if(count($data) > 0){
+            // Create Backup
+            $this->createBackup();
+
+            $env = $this->getContent();
+            
+            // Expand the existing array with the new data
+            foreach($data as $key => $value){
+                $env[$key] = $value;
+            }
+
+            return $this->save($env);
+        }
+        return false;
+    }
+
+    /**
+     * Delete one or more entries from the env file.
+     */
+    public function deleteData($data = array()){
+        $this->createBackup();
+        $env = $this->getContent();
+
+        foreach($data as $delete){
+            foreach($env as $key => $value){
+                if($delete === $key){
+                    unset($env[$key]);
+                }
+            }
+        }
+        return $this->save($env);
     }
 }
