91414445e3e663e615b5558bc9d6eee94c34c5f5
Tim Joosten <Tjoosten3@gmail.com>
2016-10-10 01:22:28 +0200

    Add function opening brace to a new file.

--- src/DotenvEditorFacade.php
+++ src/DotenvEditorFacade.php
@@ -12,7 +12,8 @@ use Illuminate\Support\Facades\Facade;
 
 class DotenvEditorFacade extends Facade
 {
-    protected static function getFacadeAccessor() {
+    protected static function getFacadeAccessor()
+    {
         return 'brotzka-dotenveditor';
     }
 }
