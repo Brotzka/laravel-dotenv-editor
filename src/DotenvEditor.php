1f55d2d16ebd87662805e37c765c15d7b7389061
Patrick Heppler <PatrickHeppler@users.noreply.github.com>
2016-11-12 11:32:32 +0100

    Update DotenvEditor.php

--- src/DotenvEditor.php
+++ src/DotenvEditor.php
@@ -31,8 +31,8 @@ class DotenvEditor
      */
     public function __construct()
     {
-        $backupPath = config('dotenveditor.backupPath',base_path() . '/.env');
-        $env = config('dotenveditor.pathToEnv',base_path() . '/resources/backups/dotenv-editor/');
+        $backupPath = config('dotenveditor.backupPath', base_path() . '/resources/backups/dotenv-editor/');
+        $env = config('dotenveditor.pathToEnv', base_path() . '/.env');
 
         if(file_exists($env)){
             $this->env = $env;
