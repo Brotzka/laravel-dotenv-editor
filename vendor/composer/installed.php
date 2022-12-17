e62b14e701413b2f30724e3eb6f6c40af0dd1d14
Ajay Thakkar <ajaythakkar500@gmail.com>
2022-12-17 04:31:26 +0000

    Fixed Str issue on DotenvEditor.php

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '843fb4a0b43738641911348bc55f78262fc53b7f',
+    'name' => 'brotzka/laravel-dotenv-editor',
+  ),
+  'versions' => 
+  array (
+    'brotzka/laravel-dotenv-editor' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '843fb4a0b43738641911348bc55f78262fc53b7f',
+    ),
+  ),
+);
